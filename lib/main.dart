425e663b48fda78465d261c54db98e4370c77d07
Abiola Adebanjo <abiola.adebanjo@andela.com>
2019-06-13 21:16:06 +0100

    Adding a Image as a Background

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,11 @@ class _StoryPageState extends State<StoryPage> {
         //TODO: Step 1 - Add background.png to this Container as a background image.
         padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
         constraints: BoxConstraints.expand(),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/background.png'),
+          ),
+        ),
         child: SafeArea(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
